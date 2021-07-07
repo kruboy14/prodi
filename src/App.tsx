c848a0f91c5706e2312aa536b0bd88c35e28bfdf
kruboy <starkkruboy@gmail.com>
2021-07-07 23:15:09 +0300

    init project file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './App.css';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/admin">
+            <Admin />
+          </Route>
+          <Route exact path="/">
+            <Site />
+          </Route>
+          <Route path="*">
+           <NotFound/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
