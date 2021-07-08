04302ef83083ac06c9e1cf6489cff7f41d7ff139
kruboy <starkkruboy@gmail.com>
2021-07-08 10:51:44 +0300

    finish footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,40 +3,82 @@ import { Link, useRouteMatch } from 'react-router-dom';
 import styles from './Header.module.scss';
 import classNames from 'classnames';
 import { useState } from 'react';
+import { useEffect } from 'react';
+
+interface IMenuItem {
+  pathname: string;
+  value: string;
+}
 
 const Header: React.FC = () => {
   const { path } = useRouteMatch();
-  const [IsBurgerOpen, setIsBurgerOpen] = useState(false)
-  const hanldeBurgerClick = () =>{
-    setIsBurgerOpen((prev) => !prev)
-  }
+  const [IsBurgerOpen, setBurgerOpen] = useState<boolean>(false);
+  const [menuItems, setMenuItems] = useState<IMenuItem[]>([
+    {
+      pathname: '/',
+      value: 'Blog',
+    },
+    {
+      pathname: '/about',
+      value: 'About',
+    },
+  ]);
+  const hanldeBurgerClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    setBurgerOpen((prev) => !prev);
+  };
+  useEffect(() => {
+    document.body.style.overflow = IsBurgerOpen ? 'hidden' : 'auto';
+  }, [IsBurgerOpen]);
   return (
-    <header className={styles.container}>
-      <div className={styles.logo}>ProDi</div>
-      <nav>
-        <ul className={styles.menu}>
-          <li
-            className={classNames({
-              [styles.menuItem]: true,
-              [styles.itemActive]: path === '/',
-            })}>
-            <Link to="/">Blog</Link>
-          </li>
-          <li
-            className={classNames({
-              [styles.menuItem]: true,
-              [styles.itemActive]: path === '/about',
-            })}>
-            <Link to="/about">About</Link>
-          </li>
+    <>
+      <header className={styles.container}>
+        <div className={styles.logo}>ProDi</div>
+        <nav>
+          <ul className={styles.menu}>
+            {menuItems.map(({ value, pathname }, index) => (
+              <li
+                key={index}
+                className={classNames({
+                  [styles.menuItem]: true,
+                  [styles.itemActive]: path === pathname,
+                })}>
+                <Link to={pathname}>{value}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          onClick={hanldeBurgerClick}
+          className={classNames({
+            [styles.burger]: true,
+            [styles.open]: IsBurgerOpen,
+          })}>
+          <span />
+          <span />
+          <span />
+        </div>
+      </header>
+      <nav
+        className={classNames({
+          [styles.burgerMenu]: true,
+          [styles.open]: IsBurgerOpen,
+        })}>
+        <ul className={styles.burgerMenuList}>
+          {menuItems.map(({ value, pathname }, index) => (
+            <li
+              key={index}
+              style={{ transition: `${index + 1 * 0.25 + 0.25}s` }}
+              className={classNames({
+                [styles.burgerMenuItem]: true,
+                [styles.open]: IsBurgerOpen,
+              })}>
+              <Link to={pathname}>{value}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
-      <div onClick={hanldeBurgerClick} className={classNames({[styles.burger]: true, [styles.open]: IsBurgerOpen})}>
-        <span />
-        <span />
-        <span />
-      </div>
-    </header>
+      <main>Lorem*100</main>
+    </>
   );
 };
 
